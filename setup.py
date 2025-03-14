71b3fad3719187ec451d10869525794fe0da85e7
Sepehr Saryazdi <sepehr.saryazdi@gmail.com>
2025-03-15 10:28:51 +1100

    Added relevant installation requirements

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=['setuptools', 'opencv-python' ,'os'],
     zip_safe=True,
     maintainer='sepehr',
     maintainer_email='sepehr.saryazdi@gmail.com',
